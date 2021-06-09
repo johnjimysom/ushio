bcfdc306f82ce1eea20516755ee89ef6dc830ee7
John-Jimi Som <johnjimy.som@student.csulb.edu>
2021-06-09 13:15:44 -0700

    updated ushio.cpp

--- ushio.cpp
+++ ushio.cpp
@@ -24,3 +24,47 @@ int main() {
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
+/*
+
+
+*/
+/***********************************************************************************************
+#include <string>
+#include <fstream>
+#include <vector>
+
+void write_csv(std::string filename, std::string colname, std::vector<int> vals){
+    // Make a CSV file with one column of integer values
+    // filename - the name of the file
+    // colname - the name of the one and only column
+    // vals - an integer vector of values
+
+    // Create an output filestream object
+    std::ofstream myFile(filename);
+
+    // Send the column name to the stream
+    myFile << colname << "\n";
+
+    // Send data to the stream
+    for(int i = 0; i < vals.size(); ++i)
+    {
+        myFile << vals.at(i) << "\n";
+    }
+
+    // Close the file
+    myFile.close();
+}
+
+int main() {
+    // Make a vector of length 100 filled with 69s
+    std::vector<int> vec(100, 69);
+
+    // Write the vector to CSV
+    write_csv("ushios.csv", "sixty-nine(s)", vec);
+
+    return 0;
+}
+
+
+
+***********************************************************************************************/
